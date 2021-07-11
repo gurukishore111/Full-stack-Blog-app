ef9d374aed428e5d3173ff943df34372c14d58fc
Guru Kishore <guru.kishore@wylo.in>
2021-07-11 08:52:22 +0530

    added not-founded page

--- crud/app/router.js
+++ crud/app/router.js
@@ -10,4 +10,5 @@ Router.map(function () {
   this.route('postDetails', { path: 'posts/:id' });
   this.route('createPost', { path: 'add/post' });
   this.route('updatePost', { path: 'edit/post/:id' });
+  this.route('notFounded', { path: '/*path' });
 });
--- crud/app/update-post/route.js
+++ crud/app/update-post/route.js
@@ -7,18 +7,26 @@ export default class UpdatePostRoute extends Route {
     const { id } = params;
     this.id = id;
     //we can using findRecord, its give get request to backend (post/:id) request,in this case ,I not have  get:id route in backend
-    const data = await this.store.findRecord('post', id);
-    console.log(data.id);
+    const data = await this.store
+      .findRecord('post', id)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        this.transitionTo('notFounded', 404);
+      });
   }
 
   // accessing the controller
   setupController(controller, model) {
     super.setupController(controller, model);
-    this.store.findRecord('post', this.id).then(function (postData) {
-      controller.set('postTitle', postData.postTitle);
-      controller.set('image', postData.image);
-      controller.set('description', postData.description);
-      controller.set('postId', postData.id);
-    });
+    if (this.id) {
+      this.store.findRecord('post', this.id).then(function (postData) {
+        controller.set('postTitle', postData.postTitle);
+        controller.set('image', postData.image);
+        controller.set('description', postData.description);
+        controller.set('postId', postData.id);
+      });
+    }
   }
 }
